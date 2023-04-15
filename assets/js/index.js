0bf44713cdfc192c848cc7065ce10d33c8bc0fdc
lsunhash24 <lsanhueza2401@gmail.comm>
2023-04-15 15:21:44 -0400

    Documentación sitio y estilos hoja de contacto

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,11 @@
-$(document).ready(function() {
 
+// declaración de jquery que permite que el bloque 
+// se ejecutará sólo cuando la página esté disponible
+
+$(document).ready(function() {
+// evento de fancybox que la dinamismo a la galeria
     Fancybox.bind('[data-fancybox="gallery"]', {
-        // Your custom options
+        
       });
     
 });
